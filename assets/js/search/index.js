73abde15fde46091f7f1deab3db78d158dfacada
mobin.2006 <mobingharibavi@gmail.com>
2024-05-24 23:43:28 +0330

    update

--- assets/js/search/index.js
+++ assets/js/search/index.js
@@ -1,6 +1,15 @@
-import './menu.js';
-import './passengers.js';
-import './radio-inputs.js';
-import './notifications.js';
-import './routes-popup.js';
-import './search-inputs.js';
+import "./menu.js";
+import "./passengers.js";
+import "./radio-inputs.js";
+import "./notifications.js";
+import "./routes-popup.js";
+import "./search-inputs.js";
+
+const Calender = document.querySelector(".calender");
+const date = document.querySelector(".Date");
+
+const showAndCloseCalender = (event) => {
+  Calender.classList.toggle("hidden");
+};
+
+date.addEventListener("click", showAndCloseCalender);
